015f70335fdcad9190bc04e0911cf2ddb809674c
vinothselvadurai <71251375+vinothselvadurai@users.noreply.github.com>
2022-06-14 09:50:39 +0530

    Update problem1.js

--- DSA-js/problem1.js
+++ DSA-js/problem1.js
@@ -1,3 +1,154 @@
+Q1
+Problem Description
+You are given a string A of size N.
+
+Return the string A after reversing the string word by word.
+
+NOTE:
+
+A sequence of non-space characters constitutes a word.
+Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+If there are multiple spaces between words, reduce them to a single space in the reversed string.
+
+
+Problem Constraints
+1 <= N <= 3 * 105
+
+
+
+Input Format
+The only argument given is string A.
+
+
+
+Output Format
+Return the string A after reversing the string word by word.
+
+
+
+Example Input
+Input 1:
+    A = "the sky is blue"
+Input 2:
+    A = "this is ib"
+
+
+Example Output
+Output 1:
+    "blue is sky the"
+Output 2:
+    "ib is this"    
+
+
+Example Explanation
+Explanation 1:
+    We reverse the string word by word so the string becomes "the sky is blue".
+Explanation 2:
+    We reverse the string word by word so the string becomes "this is ib".
+----------------------------------------------------------------------------
+Problem Description
+Given a string A, you are asked to reverse the string and return the reversed string.
+
+
+
+Problem Constraints
+1 <= |A| <= 105
+
+String A consist only of lowercase characters.
+
+
+
+Input Format
+First and only argument is a string A.
+
+
+
+Output Format
+Return a string denoting the reversed string.
+
+
+
+Example Input
+Input 1:
+
+ A = "scaler"
+Input 2:
+
+ A = "academy"
+
+
+Example Output
+Output 1:
+
+ "relacs"
+Output 2:
+
+ "ymedaca"
+
+
+Example Explanation
+Explanation 1:
+
+ Reverse the given string.
+---------------------------------------------------------------------
+
+Problem Description
+
+Given a string A, rotate the string B times in clockwise direction and return the string.
+
+
+
+Problem Constraints
+
+1 <= |A| <= 105
+
+1 <= B <= 109
+
+String A consist only of lowercase characters.
+
+
+
+Input Format
+
+First and only argument is a string A.
+
+
+
+Output Format
+
+Return a string denoting the rotated string.
+
+
+
+Example Input
+
+Input 1:
+
+ A = "scaler"
+ B = 2
+Input 2:
+
+ A = "academy"
+ B = 7
+
+
+Example Output
+
+Output 1:
+
+ "erscal"
+Output 2:
+
+ "academy"
+
+
+Example Explanation
+
+Explanation 1:
+
+ Rotate the given string twice so the string becomes "erscal".
+ 
+ -------------------------------------------------------------------------
 Q4. tolower()
 Unsolved
 character backgroundcharacter
@@ -51,3 +202,178 @@ Example Explanation
 Explanation 1:
 
  All the characters in the returned array are in lowercase alphabets.
+
+--------------------------------------------------------
+
+
+Problem Description
+You are given a function to_upper() consisting of a character array A.
+
+Convert each charater of A into Uppercase character if it exists. If the Uppercase of a character does not exist, it remains unmodified.
+The lowercase letters from a to z is converted to uppercase letters from A to Z respectively.
+
+Return the uppercase version of the given character array.
+
+
+
+Problem Constraints
+1 <= |A| <= 105
+
+
+
+Input Format
+Only argument is a character array A.
+
+
+
+Output Format
+Return the uppercase version of the given character array.
+
+
+
+Example Input
+Input 1:
+
+ A = ['S', 'c', 'A', 'L', 'E', 'r', 'A', 'c', 'a', 'D', 'e', 'm', 'y']
+Input 2:
+
+ A = ['S', 'c', 'a', 'L', 'e', 'R', '#', '2', '0', '2', '0']
+
+
+Example Output
+Output 1:
+
+ ['S', 'C', 'A', 'L', 'E', 'R', 'A', 'C', 'A', 'D', 'E', 'M', 'Y']
+Output 2:
+
+ ['S', 'C', 'A', 'L', 'E', 'R', '#', '2', '0', '2', '0']
+
+
+Example Explanation
+Explanation 1:
+
+ All the characters in the returned array are in uppercase alphabets.
+ 
+ ------------------------------------------------------------------------
+
+Q6. Isalnum()
+Unsolved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA and get it resolved.
+Get help from TA.
+Problem Description
+You are given a function isalpha() consisting of a character array A.
+
+Return 1 if all the characters of a character array are alphanumeric (a-z, A-Z, and 0-9) else, return 0.
+
+
+
+Problem Constraints
+1 <= |A| <= 105
+
+
+
+Input Format
+Only argument is a character array A.
+
+
+
+Output Format
+Return 1 if all the characters of the character array are alphanumeric (a-z, A-Z and 0-9), else return 0.
+
+
+
+Example Input
+Input 1:
+
+ A = ['S', 'c', 'a', 'l', 'e', 'r', 'A', 'c', 'a', 'd', 'e', 'm', 'y', '2', '0', '2', '0']
+Input 2:
+
+ A = ['S', 'c', 'a', 'l', 'e', 'r', '#', '2', '0', '2', '0']
+
+
+Example Output
+Output 1:
+
+ 1
+Output 2:
+
+ 0
+
+
+Example Explanation
+Explanation 1:
+
+ All the characters are alphanumeric.
+Explanation 2:
+
+ All the characters are NOT alphabets i.e ('#').
+ 
+ -----------------------------------------------------------
+
+
+Q7. Isalpha()
+Unsolved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA and get it resolved.
+Get help from TA.
+Problem Description
+
+You are given a function isalpha() consisting of a character array A.
+
+Return 1 if all the characters of the character array are alphabetical (a-z and A-Z), else return 0.
+
+
+
+Problem Constraints
+
+1 <= |A| <= 105
+
+
+
+Input Format
+
+Only argument is a character array A.
+
+
+
+Output Format
+
+Return 1 if all the characters of the character array are alphabetical (a-z and A-Z), else return 0.
+
+
+
+Example Input
+
+Input 1:
+
+ A = ['S', 'c', 'a', 'l', 'e', 'r', 'A', 'c', 'a', 'd', 'e', 'm', 'y']
+Input 2:
+
+ A = ['S', 'c', 'a', 'l', 'e', 'r', '#', '2', '0', '2', '0']
+
+
+Example Output
+
+Output 1:
+
+ 1
+Output 2:
+
+ 0
+
+
+Example Explanation
+
+Explanation 1:
+
+ All the characters are alphabets.
+Explanation 2:
+
+ All the characters are NOT alphabets i.e ('#', '2', '0', '2', '0').
+ 
+ ----------------------------------------------------
+
+
